bfdff4a13afe27f04089c42f602da77b0b255688
AnnsAnn <git@annsann.eu>
2024-10-02 16:56:36 +0200

    Docs: New Networking

---
+++ group__transportlayer.js
@@ -0,0 +1,5 @@
+var group__transportlayer =
+[
+    [ "TCP", "group__net__tcp.html", "group__net__tcp" ],
+    [ "UDP", "group__net__udp.html", "group__net__udp" ]
+];
--- topics.js
+++ topics.js
@@ -1,5 +1,12 @@
 var topics =
 [
+    [ "Features", "index.html#autotoc_md2180", null ],
+    [ "Getting RIOT", "index.html#autotoc_md2181", null ],
+    [ "Getting Started", "index.html#autotoc_md2182", null ],
+    [ "Forum", "index.html#autotoc_md2183", null ],
+    [ "Contribute", "index.html#autotoc_md2184", null ],
+    [ "Mailing Lists", "index.html#autotoc_md2185", null ],
+    [ "License", "index.html#autotoc_md2186", null ],
     [ "Boards", "group__boards.html", "group__boards" ],
     [ "Bootloaders", "group__bootloaders.html", "group__bootloaders" ],
     [ "C++", "group__cpp.html", "group__cpp" ],
@@ -8,7 +15,7 @@ var topics =
     [ "Drivers", "group__drivers.html", "group__drivers" ],
     [ "Generic pseudomodules", "group__pseudomodules.html", "group__pseudomodules" ],
     [ "Kernel", "group__core.html", "group__core" ],
-    [ "Networking", "group__net.html", "group__net" ],
+    [ "Networking", "group__network.html", "group__network" ],
     [ "Packages", "group__pkg.html", "group__pkg" ],
     [ "System", "group__sys.html", "group__sys" ],
     [ "Utilities", "group__utils.html", "group__utils" ]
